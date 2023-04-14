a2517d4a617e45b3b333fc3e966e2ea7eaeca759
kamal7643 <67092345+kamal7643@users.noreply.github.com>
2023-04-14 18:56:10 +0530

    BTP -2 done

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ctypes import *
-
+import time
 
 so_file="/Users/kamalswami/Documents/ABE/cpp_cpabe_2/app.so"
 
@@ -8,8 +8,36 @@ app = CDLL(so_file)
 def pass_string(str):
     return create_string_buffer(str.encode('utf-8'))
 
+import time
 
-app.setup()
-app.enc(pass_string(".ABE_DIR/files/doc.txt"), pass_string(".ABE_DIR/encryption/doc.txt.cpabe"))
-app.keygen()
-app.dec(pass_string(".ABE_DIR/encryption/doc.txt.cpabe"), pass_string(".ABE_DIR/decryption/doc.txt"))
+# app.keygen()
+
+t1 = time.time()
+app.dec(pass_string(".ABE_DIR/encryption/output.dat.cpabe"), pass_string(".ABE_DIR/decryption/output.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+app.dec(pass_string(".ABE_DIR/encryption/output1.dat.cpabe"), pass_string(".ABE_DIR/decryption/output1.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+app.dec(pass_string(".ABE_DIR/encryption/output2.dat.cpabe"), pass_string(".ABE_DIR/decryption/output2.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+app.dec(pass_string(".ABE_DIR/encryption/output3.dat.cpabe"), pass_string(".ABE_DIR/decryption/output3.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+app.dec(pass_string(".ABE_DIR/encryption/output4.dat.cpabe"), pass_string(".ABE_DIR/decryption/output4.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+app.dec(pass_string(".ABE_DIR/encryption/output5.dat.cpabe"), pass_string(".ABE_DIR/decryption/output5.dat"))
+print(time.time()-t1)
+t1=time.time()
+
+# app.setup()
+# app.enc(pass_string(".ABE_DIR/files/doc.txt"), pass_string(".ABE_DIR/encryption/doc.txt.cpabe"))
+# app.keygen()
+# app.dec(pass_string(".ABE_DIR/encryption/doc.txt.cpabe"), pass_string(".ABE_DIR/decryption/doc.txt"))
